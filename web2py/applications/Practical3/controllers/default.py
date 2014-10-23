32516263534705674b1473f244dcb49ffd9e3318
Rob Kerry <rck504@york.ac.uk>
2014-10-23 17:45:18 +0100

    Practical 3
    Finished Part 1

--- web2py/applications/Practical3/controllers/default.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @IAPT: Very simple controller
-
-def index():
-    return dict(features=db(db.Products.id==db.Features.product_id).select())
-
-def search():
-    return dict(request.vars)
